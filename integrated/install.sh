64d30360f6a3eccaef4b82b94d337d7e7785ff4d
nicolas-rabault <nicolas.rabault@inria.fr>
2016-04-18 19:45:34 +0200

    Add a small sudo

--- integrated/install.sh
+++ integrated/install.sh
@@ -3,7 +3,7 @@
 #description     :This script install toochain for RPI.
 #source          :http://www.blaess.fr/christophe/2014/03/06/compilation-native-de-modules-k$
 
-apt-get -y install bc git libncurses5-dev
+sudo apt-get -y install bc git libncurses5-dev
 
 git clone https://github.com/raspberrypi/tools
 
